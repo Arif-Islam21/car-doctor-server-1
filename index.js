fa5a3b898f60141c6143dbca18d472b4a0961a2e
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-27 09:54:06 +0600

    set token to the cookie

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
-console.log(process.env.DB_PASS);
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n7e36sw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -39,7 +44,13 @@ async function run() {
         expiresIn: "1h",
       });
 
-      res.send({ token });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
     });
 
     // services related apis
